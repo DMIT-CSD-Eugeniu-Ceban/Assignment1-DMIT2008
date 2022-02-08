4e7a152c80fb70d6a846d7dba2542ac1391bf25f
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2022-02-08 12:53:39 -0700

    last update

--- src/js/update.js
+++ src/js/update.js
@@ -3,14 +3,17 @@ import { ref as databaseRef, set, get, } from 'firebase/database';
 import { db, storage } from "./libs/firebase/firebaseConfig";
 
 const cardForm = document.forms['cardForm']
+let productInfo;
+const key = sessionStorage.getItem('key');
 
 async function pageInit() {
-    const key = sessionStorage.getItem('key');
     const productRef = databaseRef(db, `products/${key}`)
     const productSnapShot = await get(productRef)
 
+
     if (productSnapShot.exists()) {
-        setFieldValues(productSnapShot.val())
+        productInfo = productSnapShot.val()
+        setFieldValues(productInfo)
     }
 
     cardForm.addEventListener('submit', onUpdateProduct)
@@ -33,26 +36,32 @@ function setFieldValues({ urlPath, type, name, message, price }) {
 
 
 async function updateProductData() {
+    const sku = productInfo.sku
     const type = cardForm.elements['shoesType'].value.trim();
     const name = cardForm.elements['shoesName'].value.trim();
     const price = cardForm.elements['shoesPrice'].value.trim();
     const message = cardForm.elements['message'].value.trim();
-
+    let filePath = productInfo.key.val /* I stopped here */
 
     const file = cardForm.elements['productImage'].files;
-    if (file.lenght !== 0) {
-        const imageRef = storageRef(storage, `product-images/${file.name}`)
-        const uploadResult = await uploadBytes(imageRef, file);
-        const imagePath = uploadResult.metadata.fullPath;
-        const urlPath = await getDownloadURL(imageRef);
-    }
+
+    if (file.length !== 0) {
+        const imageRef = storageRef(storage, `product-images/${sku}`)
+        
+     }
+
+    const uploadResult = await uploadBytes(imageRef, file);
+    const imagePath = uploadResult.metadata.fullPath;
+    const urlPath = await getDownloadURL(imageRef);
 
 
-    const key = sessionStorage.getItem('key');
+    /* const key = sessionStorage.getItem('key'); */
+
     const dataRef = databaseRef(db, `products/${key}`)
 
     set(dataRef, {
         key,
+        sku: `ecsp${itemRef.key}`,
         urlPath,
         message,
         name,
@@ -63,3 +72,5 @@ async function updateProductData() {
 }
 
 pageInit()
+
+
--- src/js/write.js
+++ src/js/write.js
@@ -27,27 +27,28 @@ async function uploadNewProduct() {
     const name = document.querySelector('#shoesName').value.trim();
     const message = document.querySelector('#message').value.trim();
     const price = document.querySelector('#shoesPrice').value.trim();
-
+    
     // Write data to the Firebase
-    const imageRef = storageRef(storage, `product-images/${file.name}`);
     const dataRef = databaseRef(db, 'products')
+    const itemRef = await push(dataRef)
+    const sku = `ecsp${itemRef.key}`
+    const imageRef = storageRef(storage, `product-images/${sku}`);
 
     const uploadResult = await uploadBytes(imageRef, file);
     const urlPath = await getDownloadURL(imageRef)
     const imagePath = uploadResult.metadata.fullPath;
 
     // firebase unique key
-    const itemRef = await push(dataRef)
 
     set(itemRef, {
         key: itemRef.key,
-        sku: `ecsp${itemRef.key}`,
+        sku,
         urlPath,
         imagePath,
         type,
         name,
         message,
-        price,
+        price
         /* size: [5, 6, 7, 8, 9, 10, 11, 12] */
     })
 
